c45bfe3896a77b2b1b8dae01f46499131d03099f
Cristian Rios <cristian.rios@moxit.net>
2019-11-29 22:01:16 -0300

    Division en funciones

--- Ejercicio 2/definiciones.h
+++ Ejercicio 2/definiciones.h
@@ -1,8 +1,12 @@
 #define LINE_LEN 30
-#define CON_MSJ 1
+#define CON_MSJ 2
 #define ON_FILE_ERROR -2
 #define INVALID_DATA -3
 #define PATH_LEN 300
+#define THREAD_LOAD_ERROR -1
+#define THREAD_DOWNLOAD_ERROR -4
+#define REMAINING_CALC_ERROR -5
+#define OP_SUCESS 1
 
 typedef struct{
     double primero;
--- Ejercicio 2/main.c
+++ Ejercicio 2/main.c
@@ -12,6 +12,9 @@ Nuñez Diego - DNI 36.159.969
 #include "main.h"
 
 void calcularThreads(const int threads, const int nLineas, int* nThreads, int* cargaPThread, int* cargaPProcess);
+int cargaEnThreads(FILE* filesrc, tColaThread* colaThreads, const int nThreads, const int cargaPThread);
+int descargaThreadsEnArchivo(FILE* filedst, tColaThread* colaThreads, const int nThreads, const int cargaPThread);
+int calcularRestantesEnProceso(FILE* filesrc, FILE* filedst, const int cargaPProcess);
 
 FILE* filesrc;
 FILE* filedst;
@@ -34,22 +37,18 @@ void *threadCalc(void* param){
 }
 
 int main(int argc, char *argv[]) {
-    char line[LINE_LEN], *aux;
+    char line[LINE_LEN];
     char fileIn[PATH_LEN];
     char fileOut[PATH_LEN];
-    double resultado;
     int threads = 0;
     int nLineas = 0;
     int nThreads = 0;
     int cargaPProcess = 0;
-    int iThread;
-    int iCargaThread;
-    t_par auxPar;
 
     validateParams(argc, argv, &threads, fileIn, fileOut);
     
-    printf("%s\n", fileIn);
-    printf("%s\n", fileOut);
+    printf("Archivo de entrada: %s\n", fileIn);
+    printf("Archivo de saslida: %s\n", fileOut);
 
     if(!abrirArchivo(&filesrc, fileIn,"r")) {
         perror("Error abriendo archivo de datos de vectores. ");
@@ -70,36 +69,60 @@ int main(int argc, char *argv[]) {
 
     calcularThreads(threads, nLineas, &nThreads, &cargaPThread, &cargaPProcess);
 
-    rewind(filesrc);
-    rewind(filedst);
-
-    pthread_t thread_id;
     tColaThread colaThreads;
-    tColaCalc* auxColaCalc;
-    tInfoCalc* auxInfoCalc;
-    tInfoThread* auxInfoThread;
     crearColaThread(&colaThreads);
 
-    tInfoCalc auxInfoCalcProcess;
+    if (cargaEnThreads(filesrc, &colaThreads, nThreads, cargaPThread) == THREAD_LOAD_ERROR) {
+        fprintf(stderr, "Error durante la carga de threads\n");
+        fprintf(stderr, "Se ha llegado al fin de archivo antes de lo esperado\n");
+        fclose(filesrc);
+        fclose(filedst);
+        exit(THREAD_LOAD_ERROR);
+    }
+
+    if (descargaThreadsEnArchivo(filedst, &colaThreads, nThreads, cargaPThread) == THREAD_DOWNLOAD_ERROR) {
+        fprintf(stderr, "Error durante la descarga de threads al archivo\n");
+        fclose(filesrc);
+        fclose(filedst);
+        exit(THREAD_DOWNLOAD_ERROR);
+    }
+
+    if (calcularRestantesEnProceso(filesrc, filedst, cargaPProcess) == REMAINING_CALC_ERROR) {
+        fprintf(stderr, "Error durante los calculos restantes en el proceso\n");
+        fclose(filesrc);
+        fclose(filedst);
+        exit(REMAINING_CALC_ERROR);
+    }
+    
+    fclose(filesrc);
+    fclose(filedst);
+
+    return 0;
+}
+
+int cargaEnThreads(FILE* filesrc, tColaThread* colaThreads, const int nThreads, const int cargaPThread){
+    rewind(filesrc);
+
+    tInfoThread* auxInfoThread;
+    tColaCalc* auxColaCalc;
+    tInfoCalc* auxInfoCalc;
+    char line[LINE_LEN];
+    t_par auxPar;
 
     for (int i = 0; i < nThreads; i++)
     {
+        auxInfoThread = (tInfoThread*) malloc(sizeof(tInfoThread));
+
         auxColaCalc = (tColaCalc*) malloc(sizeof(tColaCalc));
         crearColaCalc(auxColaCalc);
-
-        auxInfoThread = (tInfoThread*) malloc(sizeof(tInfoThread));
         
         for (int j = 0; j < cargaPThread; j++)
         {
             auxInfoCalc = (tInfoCalc*) malloc(sizeof(tInfoCalc));
             
-            if(!fgets(line, sizeof(line), filesrc)){
-                fprintf(stderr, "Error durante la carga de threads\n");
-                fprintf(stderr, "Se ha llegado al fin de archivo antes de lo esperado\n");
-                fclose(filesrc);
-                fclose(filedst);
-                return -1;
-            }
+            if(!fgets(line, sizeof(line), filesrc))
+                return THREAD_LOAD_ERROR;
+            
             trozarCampos(&auxPar, line);
             auxInfoCalc->primero = auxPar.primero;
             auxInfoCalc->segundo = auxPar.segundo;
@@ -109,19 +132,30 @@ int main(int argc, char *argv[]) {
 
         pthread_create(&(auxInfoThread->thread_id), NULL, threadCalc, auxColaCalc);
 
-        ponerEnColaThread(&colaThreads, auxInfoThread);
+        ponerEnColaThread(colaThreads, auxInfoThread);
     }
 
+    return OP_SUCESS;
+}
+
+int descargaThreadsEnArchivo(FILE* filedst, tColaThread* colaThreads, const int nThreads, const int cargaPThread){
+    rewind(filedst);
+    tInfoThread* auxInfoThread;
+    tColaCalc* auxColaCalc;
+    tInfoCalc* auxInfoCalc;
+    
     for (int i = 0; i < nThreads; i++)
     {
-        sacarDeColaThread(&colaThreads, &auxInfoThread);
+        sacarDeColaThread(colaThreads, &auxInfoThread);
         pthread_join(auxInfoThread->thread_id, (void**)&(auxColaCalc));
 
         for (int j = 0; j < cargaPThread; j++)
         {
             sacarDeColaCalc(auxColaCalc, &auxInfoCalc);
-            printf("Recibido del thread n# %d el resultado %f\n", i+1, auxInfoCalc->resultado);
-            fprintf(filedst, "%f\n", auxInfoCalc->resultado);
+            // printf("Recibido del thread n# %d el resultado %f\n", i+1, auxInfoCalc->resultado);
+            if(fprintf(filedst, "%f\n", auxInfoCalc->resultado) < 0)
+                return THREAD_DOWNLOAD_ERROR;
+            
             free(auxInfoCalc);
         }
 
@@ -129,40 +163,35 @@ int main(int argc, char *argv[]) {
         free(auxInfoThread);
     }
 
+    return OP_SUCESS;
+}
 
-    // Calculos restantes para el proceso
+int calcularRestantesEnProceso(FILE* filesrc, FILE* filedst, const int cargaPProcess){
+    t_par auxPar;
+    char line[LINE_LEN];
+    tInfoCalc auxInfoCalcProcess;
+    
     for (int i = 0; i < cargaPProcess; i++)
     {
-        if(!fgets(line, sizeof(line), filesrc)){
-            fprintf(stderr, "Error durante los calculos restantes en el proceso\n");
-            fprintf(stderr, "Se ha llegado al fin de archivo antes de lo esperado\n");
-            fclose(filesrc);
-            fclose(filedst);
-            return -1;
-        }
+        if(!fgets(line, sizeof(line), filesrc))
+            return REMAINING_CALC_ERROR;
+        
         trozarCampos(&auxPar, line);
         auxInfoCalcProcess.primero = auxPar.primero;
         auxInfoCalcProcess.segundo = auxPar.segundo;
         calcular(&auxInfoCalcProcess);
-        printf("Desde el proceso, escribiendo el resultado %f\n", auxInfoCalcProcess.resultado);
-        fprintf(filedst, "%f\n", auxInfoCalcProcess.resultado);
+        // printf("Desde el proceso, escribiendo el resultado %f\n", auxInfoCalcProcess.resultado);
+        if(fprintf(filedst, "%f\n", auxInfoCalcProcess.resultado) < 0)
+            return REMAINING_CALC_ERROR;
     }
-    
-    fclose(filesrc);
-    fclose(filedst);
 
-    return 0;
+    return OP_SUCESS;
 }
 
 void calcularThreads(const int threads, const int lineas, int* nThreads, int* cargaPThread, int* cargaPProcess){
     printf("Cantidad de lineas en archivo %d\n", lineas);
     
-    *nThreads = (threads > 0 && lineas >= threads)? threads : 0;
-    // if(threads == 0)
-    //     *nThreads = 0;
-    // else if(lineas >= threads){
-    //     *nThreads = threads;
-    // }
+    *nThreads = (threads > 0 && lineas >= threads) ? threads : 0;
 
     printf("Numero de threads necesitados: %d\n", *nThreads);
     if(*nThreads != 0){
